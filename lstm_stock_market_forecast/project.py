955c557cc02088bfd0aa38bdd967c3455e4a67d9
tomas.araujo <tomas.araujo@ltplabs.com>
2025-12-26 17:44:44 +0000

    IBM stock is way above prediction ahah

--- lstm_stock_market_forecast/project.py
+++ lstm_stock_market_forecast/project.py
@@ -260,3 +260,78 @@ for epoch in range(config["training"]["num_epoch"]):
     print('Epoch[{}/{}] | loss train:{:.6f}, test:{:.6f} | lr:{:.6f}'
               .format(epoch+1, config["training"]["num_epoch"], loss_train, loss_val, lr_train))
     
+
+# here we re-initialize dataloader so the data doesn't shuffled, so we can plot the values by date
+
+train_dataloader = DataLoader(dataset_train, batch_size=config["training"]["batch_size"], shuffle=False)
+val_dataloader = DataLoader(dataset_val, batch_size=config["training"]["batch_size"], shuffle=False)
+
+model.eval()
+
+# predict on the training data, to see how well the model managed to learn and memorize
+
+predicted_train = np.array([])
+
+for idx, (x, y) in enumerate(train_dataloader):
+    x = x.to(config["training"]["device"])
+    out = model(x)
+    out = out.cpu().detach().numpy()
+    predicted_train = np.concatenate((predicted_train, out))
+
+# predict on the validation data, to see how the model does
+
+predicted_val = np.array([])
+
+for idx, (x, y) in enumerate(val_dataloader):
+    x = x.to(config["training"]["device"])
+    out = model(x)
+    out = out.cpu().detach().numpy()
+    predicted_val = np.concatenate((predicted_val, out))
+
+# prepare data for plotting
+
+to_plot_data_y_train_pred = np.zeros(num_data_points)
+to_plot_data_y_val_pred = np.zeros(num_data_points)
+
+to_plot_data_y_train_pred[config["data"]["window_size"]:split_index+config["data"]["window_size"]] = scaler.inverse_transform(predicted_train)
+to_plot_data_y_val_pred[split_index+config["data"]["window_size"]:] = scaler.inverse_transform(predicted_val)
+
+to_plot_data_y_train_pred = np.where(to_plot_data_y_train_pred == 0, None, to_plot_data_y_train_pred)
+to_plot_data_y_val_pred = np.where(to_plot_data_y_val_pred == 0, None, to_plot_data_y_val_pred)
+
+# plots
+
+fig = figure(figsize=(25, 5), dpi=80)
+fig.patch.set_facecolor((1.0, 1.0, 1.0))
+plt.plot(data_date, data_close_price, label="Actual prices", color=config["plots"]["color_actual"])
+plt.plot(data_date, to_plot_data_y_train_pred, label="Predicted prices (train)", color=config["plots"]["color_pred_train"])
+plt.plot(data_date, to_plot_data_y_val_pred, label="Predicted prices (validation)", color=config["plots"]["color_pred_val"])
+plt.title("Compare predicted prices to actual prices")
+xticks = [data_date[i] if ((i%config["plots"]["xticks_interval"]==0 and (num_data_points-i) > config["plots"]["xticks_interval"]) or i==num_data_points-1) else None for i in range(num_data_points)] # make x ticks nice
+x = np.arange(0,len(xticks))
+plt.xticks(x, xticks, rotation='vertical')
+plt.grid(which='major', axis='y', linestyle='--')
+plt.legend()
+plt.show()
+
+
+
+# prepare data for plotting the zoomed in view of the predicted prices (on validation set) vs. actual prices
+
+to_plot_data_y_val_subset = scaler.inverse_transform(data_y_val)
+to_plot_predicted_val = scaler.inverse_transform(predicted_val)
+to_plot_data_date = data_date[split_index+config["data"]["window_size"]:]
+
+# plots
+
+fig = figure(figsize=(25, 5), dpi=80)
+fig.patch.set_facecolor((1.0, 1.0, 1.0))
+plt.plot(to_plot_data_date, to_plot_data_y_val_subset, label="Actual prices", color=config["plots"]["color_actual"])
+plt.plot(to_plot_data_date, to_plot_predicted_val, label="Predicted prices (validation)", color=config["plots"]["color_pred_val"])
+plt.title("Zoom in to examine predicted price on validation data portion")
+xticks = [to_plot_data_date[i] if ((i%int(config["plots"]["xticks_interval"]/5)==0 and (len(to_plot_data_date)-i) > config["plots"]["xticks_interval"]/6) or i==len(to_plot_data_date)-1) else None for i in range(len(to_plot_data_date))] # make x ticks nice
+xs = np.arange(0,len(xticks))
+plt.xticks(xs, xticks, rotation='vertical')
+plt.grid(which='major', axis='y', linestyle='--')
+plt.legend()
+plt.show()
